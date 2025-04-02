cde70cdf93f8f93d96b37743d2c7d7b04f02b2c2
Mrbysco <Mrbysco@users.noreply.github.com>
2025-04-02 15:45:15 +0200

    Port to 1.21.5

--- forge/src/main/java/com/mrbysco/undergroundvillages/datagen/StructureUpdater.java
+++ forge/src/main/java/com/mrbysco/undergroundvillages/datagen/StructureUpdater.java
@@ -79,7 +79,7 @@ public class StructureUpdater implements DataProvider {
 
 	private static CompoundTag updateNBT(CompoundTag nbt) {
 		final CompoundTag updatedNBT = DataFixTypes.STRUCTURE.updateToCurrentVersion(
-				DataFixers.getDataFixer(), nbt, nbt.getInt("DataVersion")
+				DataFixers.getDataFixer(), nbt, nbt.getIntOr("DataVersion", 0)
 		);
 		StructureTemplate template = new StructureTemplate();
 		template.load(BuiltInRegistries.BLOCK, updatedNBT);
